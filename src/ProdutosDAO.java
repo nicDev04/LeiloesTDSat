bc6c12e14c3b7d0237d4a453e683346ea202a212
Nicolas <contato.nicolas04@gmail.com>
2025-05-26 17:54:32 -0300

    listagem de cadastrados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -30,7 +30,27 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
-
+        
+        try{
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+        
+        while(resultset.next()){
+            ProdutosDTO prod = new ProdutosDTO();
+            prod.setId(resultset.getInt("id"));
+            prod.setNome(resultset.getString("nome"));
+            prod.setValor(resultset.getInt("valor"));
+            prod.setStatus(resultset.getString("status"));
+            listagem.add(prod);
+            
+        }
+        
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro no cadastro");
+        }
+        
         return listagem;
     }
 
